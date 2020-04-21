60ebabb1284d3c39130e4c844bcb5c240bd47b16
Arther <arther.tony02@gmail.com>
2020-04-21 09:53:43 +0530

    Find if vertex is in border

--- lib/vertex.ex
+++ lib/vertex.ex
@@ -15,26 +15,55 @@ defmodule DotSquare.Vertex do
     is_already_marked?(pair, tail, head.pair == pair)
   end
 
-  defp is_valid_pair?(0, _, 1), do: false
+  defp valid_pair?(0, _, 1), do: false
 
-  defp is_valid_pair?(_, 0, 1), do: false
+  defp valid_pair?(_, 0, 1), do: false
 
-  defp is_valid_pair?(0, 0, 5), do: true
+  defp valid_pair?(0, 0, 5), do: true
 
-  defp is_valid_pair?(x, x, 5), do: true
+  defp valid_pair?(x, x, 5), do: true
 
-  defp is_valid_pair?(_, _, 1), do: true
+  defp valid_pair?(_, _, 1), do: true
 
-  def add_vertix(vertices, {a, b} = pair, player, size) do
-    if(is_valid_pair?(rem(a, size), rem(b, size), b - a)) do
+  defp valid_pair?({a, b} = _pair, size) do
+    valid_pair?(rem(a, size), rem(b, size), b - a)
+  end
+
+  def add_vertix(vertices, pair, player, size) do
+    if(valid_pair?(pair, size)) do
       {:ok, vertices ++ [%__MODULE__{pair: pair, player: player}]}
     else
       {:error, vertices}
     end
   end
 
-  def square_formed?(vertices, start_point, end_point, size) do
+  defp row?({a, b} = _pair) do
+    b - a == 1
+  end
 
+  defp col?({a, b} = _pair, size) do
+    b - a == size
+  end
+
+  def border({a, _b} = _pair, size, false = _row?) do
+    case (rem(a, size)) do
+      0 ->  :right
+      1 -> :left
+      _ -> :none
+    end
+  end
+
+  def border({a, b} = _pair, size, true = _row?) do
+    last_row = size * (size - 1)
+    case (a) do
+      x  when x < size -> :top
+      x when x > last_row -> :bottom
+      _ -> :none
+    end
+  end
+
+  def square_formed?(vertices, pair, size) do
+    row?(pair)
   end
 
 end
--- test/vertex_test.exs
+++ test/vertex_test.exs
@@ -15,4 +15,15 @@ defmodule DotSquare.VertexTest do
     assert Vertex.is_already_marked?({1,2}, vertices, false) == true
     assert Vertex.is_already_marked?({2,6}, vertices, false) == false
   end
+
+  test "border find test" do
+    assert Vertex.border({21, 22}, 5, true) == :bottom
+    assert Vertex.border({2, 3}, 5, true) == :top
+
+    assert Vertex.border({5, 10}, 5, false) == :right
+    assert Vertex.border({6, 11}, 5, false) == :left
+
+    assert Vertex.border({6, 7}, 5, true) == :none
+    assert Vertex.border({12, 17}, 5, false) == :none
+  end
 end
